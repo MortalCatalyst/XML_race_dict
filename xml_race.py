585a0407a606af3facc4d5f76818aa6261035470
mortalcatalyst <saythjwrenshaw@outlook.com>
2017-04-10 22:47:14 +1000

    add primary key

--- xml_race.py
+++ xml_race.py
@@ -1,4 +1,3 @@
-# import csv
 import sqlite3
 import xmltodict
 from lxml import etree
@@ -56,8 +55,8 @@ def horse(race_string):
 with open('RAND.xml', "rb") as f, sqlite3.connect("race.db") as connection:
     c = connection.cursor()
     c.execute(
-        """CREATE TABLE IF NOT EXISTS race(R_Number	INT, R_KEY INT, R_NAME TEXT, R_AGE INT, \
-        R_DIST TEXT, R_CLASS, M_ID INT)""")
+        """CREATE TABLE IF NOT EXISTS race(RaceID INT PRIMARY KEY,R_Number	INT, R_KEY INT,\
+        R_NAME TEXT, R_AGE INT, R_DIST TEXT, R_CLASS, M_ID INT)""")
 
     parser = etree.XMLParser(remove_comments=True)
     tree = etree.parse(f, parser=parser)
@@ -72,41 +71,8 @@ with open('RAND.xml', "rb") as f, sqlite3.connect("race.db") as connection:
     meeting_key = meet_key(str_tree)
     for item in b:
         item.insert(0, meeting_key)
-    # c.executemany('INSERT into race VALUES(?,?,?,?,?,?,?)', a)
-    # writer = csv.writer(g)
 
-    # writer.writerow(
-    #     ('meeting_id', 'venue', 'trackcondition', 'date', 'race_number',
-    #      'race_id', 'age', 'distance', 'class', 'horse', 'barrier', 'weight',
-    #      'rating', 'starts_gt', 'wins_gt', 'second_gt', 'third_gt'))
-
-    # MY_ITERABLE = xmltodict.parse(str_tree)
-    # for key in MY_ITERABLE.get('meeting', {}).get('race'):
-    #     for key in MY_ITERABLE.items():
-    #         meet_output = (key[1]['@id'], key[1]['@venue'],
-    #                        key[1]['@trackcondition'].strip(), key[1]['@date'],
-    #                        key[1]['@rail'])
-    #         for race_key in MY_ITERABLE.get('meeting', {}).get('race'):
-    #             race_output = (race_key['@number'], race_key['@id'],
-    #                            race_key['@shortname'],
-    #                            race_key['@age'].strip(), race_key['@distance'],
-    #                            race_key['@class'].strip())
-    #             # race_output += (key[1]['@id'], )
-    #             for nkey in race_key.get('nomination', {}):
-    #                 horse_output = (nkey['@horse'], nkey['@barrier'],
-    #                                 nkey['@weight'], nkey['@rating'],
-    #                                 nkey['@goodtrack'].split("-"))
-    #                 horseName = nkey['@horse']
-    #                 barrier = nkey['@barrier']
-    #                 weight = nkey['@weight']
-    #                 rating = nkey['@rating']
-    #                 print(race_output)
 
 pp = pprint.PrettyPrinter(indent=4, width=100)
 pp.pprint(b)
-# output = (meet_output[0], meet_output[1], meet_output[2],
-#           meet_output[3], race_output[0], race_output[1],
-#           race_output[3], race_output[4], race_output[5],
-#           horse_output[0], horse_output[1],
-#           horse_output[2], horse_output[3],
-#           horse_output[4][0], horse_output[4][1],
+
